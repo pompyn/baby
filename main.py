492eb719ba3b3026fadcf1c53d7e5f0bfc8d85a4
pompyn <pompyn@live.com>
2021-11-25 22:03:20 -0600

    add choice import, radomize questions

---
+++ main.py
@@ -0,0 +1,7 @@
+from random import choice
+questions = ['Why is the sky blue? ', 'Why do birds sing? ', 'Why do the stars shine? ']
+question = choice(questions)
+answer = input(question).lower().strip()
+while answer != 'just because':
+    answer = input("Why ").strip().lower()
+
